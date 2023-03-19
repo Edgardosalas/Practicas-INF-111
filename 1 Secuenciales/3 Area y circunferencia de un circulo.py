b069549cede974c397f653b189459a1758c716e9
Edgardosalas <edgarsalasbirrueta@gmail.com>
2023-03-18 22:00:33 -0400

    Todo Secuenciales

--- 1 Secuenciales/3 Area y circunferencia de un circulo.py
+++ 1 Secuenciales/3 Area y circunferencia de un circulo.py
@@ -1,3 +1,8 @@
 #Escribe un programa que lea el radio de un círculo y muestre su área y circunferencia en la pantalla. Utiliza las fórmulas siguientes:
 #Área = pi * radio^2
 #Circunferencia = 2 * pi * radio
+r=int(input("Introduzca el radio del circulo: "))
+pi=3.1416
+a=pi*r**2
+c=2*pi*r
+print("area=",round(a, 2), "circunferencia=", round(c, 2))
