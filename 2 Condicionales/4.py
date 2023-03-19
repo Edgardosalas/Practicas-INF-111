4c3bd5fe66a7fc144c9ccb886025ae6d30c6669a
Edgardosalas <edgarsalasbirrueta@gmail.com>
2023-03-19 12:22:43 -0400

    Condicionales

--- 2 Condicionales/4.py
+++ 2 Condicionales/4.py
@@ -1,1 +1,6 @@
 #Escribe un programa que solicite al usuario ingresar una letra. Si la letra ingresada es una vocal (a, e, i, o, u), el programa deberá imprimir en pantalla el mensaje "La letra ingresada es una vocal". De lo contrario, el programa deberá imprimir en pantalla el mensaje "La letra ingresada es una consonante".
+n=input("Introduzca una letra: ")
+if n=="a" or n=="e" or n=="i" or n=="o" or n=="u":
+    print("La letra ingresada es una vocal")
+else:
+    print("La letra ingresadad es una consonante")
--- 2 Condicionales/8.py
+++ 2 Condicionales/8.py
@@ -1,1 +1,8 @@
 #Escribe un programa que solicite al usuario ingresar el lenguaje de programación que está aprendiendo. Si el usuario ingresa "Python", el programa deberá imprimir en pantalla el mensaje "¡Python es un excelente lenguaje de programación! Sigue adelante". Si el usuario ingresa "JavaScript", el programa deberá imprimir en pantalla el mensaje "¡JavaScript es un lenguaje de programación popular y útil! Sigue adelante". Si el usuario ingresa cualquier otro lenguaje de programación, el programa deberá imprimir en pantalla el mensaje "¡Ese es un buen lenguaje de programación! Sigue adelante".
+n=input("Ingrese el lenguaje de programacion que esta aprendiendo ")
+if n=="Python":
+    print("¡Python es un excelente lenguaje de programacion")
+elif n=="JavaScript":
+    print("¡JavaScript es un lenguaje de programacion popular y util! Sigue adelante")
+else:
+    print("¡Ese es un bien lenguaje de programacion! Sigue adelante")
